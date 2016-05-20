7ae6c57b60f8fbb3f6be0a32a4c3df2cc7fe8d52
v-rcheng <v-rcheng@expedia.com>
2016-05-19 18:41:58 -0700

    extract view model

--- Connector/SenderTest/MainWindow.xaml.cs
+++ Connector/SenderTest/MainWindow.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using Extensions;
-using HttpRequestSender;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using ViewModels;
 
 namespace SenderTest
 {
@@ -16,62 +15,84 @@ namespace SenderTest
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = new SenderViewModel();
+        }
+
+        public SenderViewModel ViewModel
+        {
+            get
+            {
+                return DataContext as SenderViewModel;
+            }
+            set
+            {
+                DataContext = value;
+            }
         }
 
         private readonly Dictionary<string, string> RequestHeaders = new Dictionary<string, string>();
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var uri = TxtUri.Text.Trim();
-            var method = CbbMethod.SelectionBoxItem.ToString();
-            var connector = new Connector(uri, method);
-            var requestBody = TxtRequestContent.Text.Trim();
-            LoadRequestHeaders();
-            var requestContent = new RequestContent(requestBody, RequestHeaders.GetValue("Content-Type", StringComparer.OrdinalIgnoreCase))
+            try
             {
-                Headers = RequestHeaders
-            };
-            DateTime start = DateTime.Now;
-            var response = connector.Send(requestContent);
-            DateTime end = DateTime.Now;
-            LbStatusCode.Content = response.StatusCode;
-            LbDuration.Content = string.Format("{0}ms", (end - start).Milliseconds);
-            LoadResponseBody(response.Raw);
-            LoadResponseHeaders(response.Headers);
+                ViewModel.Send();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.GetBaseException().Message);
+            }
+
+            //var uri = TxtUri.Text.Trim();
+            //var method = CbbMethod.SelectionBoxItem.ToString();
+            //var connector = new Connector(uri, method);
+            //var requestBody = TxtRequestContent.Text.Trim();
+            //LoadRequestHeaders();
+            //var requestContent = new RequestContent(requestBody, RequestHeaders.GetValue("Content-Type", StringComparer.OrdinalIgnoreCase))
+            //{
+            //    Headers = RequestHeaders
+            //};
+            //DateTime start = DateTime.Now;
+            //var response = connector.Send(requestContent);
+            //DateTime end = DateTime.Now;
+            //LbStatusCode.Content = response.StatusCode;
+            //LbDuration.Content = string.Format("{0}ms", (end - start).Milliseconds);
+            //LoadResponseBody(response.Raw);
+            //LoadResponseHeaders(response.Headers);
         }
 
-        private void LoadRequestHeaders()
-        {
-            var headers = TxtRequestHeaders.Text.Trim().Split('\r', '\t');
-            foreach (var header in headers)
-            {
-                var item = header.Split(':');
-                if (item.Length == 2)
-                {
-                    var key = item[0].Replace("\n", "").Trim();
-                    RequestHeaders.AddOrUpdate(key, item[1].Trim(), StringComparer.OrdinalIgnoreCase);
-                }
-            }
-            if (!RequestHeaders.ContainsKey("content-type", StringComparer.OrdinalIgnoreCase))
-            {
-                RequestHeaders["Content-Type"] = "text/plain";
-            }
-        }
+        //private void LoadRequestHeaders()
+        //{
+        //    var headers = TxtRequestHeaders.Text.Trim().Split('\r', '\t');
+        //    foreach (var header in headers)
+        //    {
+        //        var item = header.Split(':');
+        //        if (item.Length == 2)
+        //        {
+        //            var key = item[0].Replace("\n", "").Trim();
+        //            RequestHeaders.AddOrUpdate(key, item[1].Trim(), StringComparer.OrdinalIgnoreCase);
+        //        }
+        //    }
+        //    if (!RequestHeaders.ContainsKey("content-type", StringComparer.OrdinalIgnoreCase))
+        //    {
+        //        RequestHeaders["Content-Type"] = "text/plain";
+        //    }
+        //}
 
-        private void LoadResponseBody(string content)
-        {
-            TxtResponseContent.Text = content;
-        }
+        //private void LoadResponseBody(string content)
+        //{
+        //    TxtResponseContent.Text = content;
+        //}
 
-        private void LoadResponseHeaders(IDictionary<string, string> headers)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (var header in headers)
-            {
-                sb.AppendLine(string.Format("{0}:{1}", header.Key, header.Value));
-            }
-            TxtResponseHeader.Text = sb.ToString();
-        }
+        //private void LoadResponseHeaders(IDictionary<string, string> headers)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    foreach (var header in headers)
+        //    {
+        //        sb.AppendLine(string.Format("{0}:{1}", header.Key, header.Value));
+        //    }
+        //    TxtResponseHeader.Text = sb.ToString();
+        //}
 
         private void CbbContentType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
---
+++ Connector/ViewModels/SenderViewModel.cs
@@ -0,0 +1,114 @@
+﻿using Extensions;
+using HttpRequestSender;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ViewModels
+{
+    public class SenderViewModel : BaseViewModel
+    {
+        public readonly ObservableCollection<Tuple<string, string>> RequestHeaders = new ObservableCollection<Tuple<string, string>>();
+
+        public Dictionary<string, string> ResponseHeaders { get; set; }
+        private string _uri;
+
+        public string Uri
+        {
+            get { return _uri; }
+            set
+            {
+                _uri = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string _method;
+
+        public string Method
+        {
+            get { return _method; }
+            set
+            {
+                _method = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _requestContent;
+
+        public string RequestContent
+        {
+            get { return _requestContent; }
+            set
+            {
+                _requestContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _statusCode;
+
+        public int StatusCode
+        {
+            get { return _statusCode; }
+            set
+            {
+                _statusCode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _duration;
+
+        public int Duration
+        {
+            get { return _duration; }
+            set
+            {
+                _duration = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _responseContent;
+
+        public string ResponseContent
+        {
+            get { return _responseContent; }
+            set
+            {
+                _responseContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SenderViewModel()
+        {
+            ResponseHeaders = new Dictionary<string, string>();
+        }
+
+        public void Send()
+        {
+            var connector = new Connector(Uri, Method);
+            var requestHeaders = RequestHeaders.ToDictionary(item => item.Item1, item => item.Item2);
+            var contentType = requestHeaders.GetValue("Content-Type", StringComparer.OrdinalIgnoreCase);
+            var requestContent = new RequestContent(RequestContent, contentType)
+            {
+                Headers = requestHeaders
+            };
+            var start = DateTime.Now;
+            var response = connector.Send(requestContent);
+            var end = DateTime.Now;
+            StatusCode = response.StatusCode;
+            Duration = (end - start).Milliseconds;
+            ResponseContent = response.Raw;
+            ResponseHeaders.Clear();
+            foreach (var header in response.Headers)
+            {
+                ResponseHeaders.Add(header.Key, header.Value);
+            }
+        }
+    }
+}
