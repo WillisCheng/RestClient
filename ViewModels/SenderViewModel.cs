0b685b9933c2d7af5ea92273285189caf50f6203
rcheng <v-rcheng@expedia.com>
2016-08-08 19:36:09 -0700

    ffff

--- ViewModels/SenderViewModel.cs
+++ ViewModels/SenderViewModel.cs
@@ -1,10 +1,10 @@
 ﻿using Extensions;
-using WebClient;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using HttpRequestSender;
 
 namespace ViewModels
 {
